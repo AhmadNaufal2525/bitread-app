fccf8dfb50315983f85c2f21ab3e6515b7b5adbe
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-11 10:50:35 +0700

    update custom button

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -3,25 +3,40 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String text;
   final Function() onPressed;
+  final Color? color, textColor;
   final double width;
-  final double height;
+  final double? height;
 
   const CustomButton({
     Key? key,
     required this.text,
     required this.onPressed,
     required this.width,
-    required this.height,
+    this.height,
+    this.color,
+    this.textColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return SizedBox(
-      width: width,
-      height: height,
+      width: size.width * 0.9,
+      height: height ?? size.height * 0.07,
       child: ElevatedButton(
         onPressed: onPressed,
-        child: Text(text)
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide.none,
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: textColor, fontSize: 14, fontWeight: FontWeight.w500),
+        ),
       ),
     );
   }
