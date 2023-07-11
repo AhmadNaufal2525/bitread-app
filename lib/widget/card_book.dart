16578ec4c84ff7f15abab8e0a3584a4c6585c059
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-11 13:25:57 +0700

    update card_book

--- lib/widget/card_book.dart
+++ lib/widget/card_book.dart
@@ -6,7 +6,8 @@ class BookCard extends StatelessWidget {
   final double rating;
   final String imageUrl;
 
-  const BookCard({super.key, 
+  const BookCard({
+    super.key,
     required this.title,
     required this.author,
     required this.rating,
@@ -15,61 +16,60 @@ class BookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 16.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(
-              imageUrl,
-              height: 160,
-              fit: BoxFit.cover,
-            ),
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.only(right: 16.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.asset(
+            imageUrl,
+            height: 160,
+            fit: BoxFit.cover,
           ),
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title.length > 16 ? '${title.substring(0, 16)}...' : title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 12.0),
+          Text(
+            title.length > 16 ? '${title.substring(0, 16)}...' : title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14.0,
+            ),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            author,
+            style: const TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey,
+            ),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            children: [
+              const Icon(
+                Icons.star,
+                color: Colors.orange,
+                size: 16.0,
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              author,
-              style: const TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey,
+              const SizedBox(width: 4.0),
+              Text(
+                rating.toString(),
+                style: const TextStyle(
+                  fontSize: 12.0,
+                ),
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 8.0),
-            Row(
-              children: [
-                const Icon(
-                  Icons.star,
-                  color: Colors.orange,
-                  size: 16.0,
-                ),
-                const SizedBox(width: 4.0),
-                Text(
-                  rating.toString(),
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),]
-      );
+            ],
+          ),
+        ],
+      ),
+    ]);
   }
 }
