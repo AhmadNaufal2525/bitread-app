38a51939b3186ddc2b95ad18f3659d0e01671d85
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-18 12:17:57 +0700

    connect to firebase for add_post_screen

--- lib/screen/add_post_screen.dart
+++ lib/screen/add_post_screen.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
 import 'package:bitread_app/widget/custom_button.dart';
+import 'package:bitread_app/widget/failed_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
+import '../widget/success_dialog.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -12,7 +18,49 @@ class AddPostScreen extends StatefulWidget {
 
 class AddPostScreenState extends State<AddPostScreen> {
   String selectedImagePath = '';
-  final _formKey = GlobalKey<FormState>();
+  late String judul;
+  late String isiBlog;
+  final formKey = GlobalKey<FormState>();
+
+  Future<void> addNewPost() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      String? userId = user?.uid;
+      String? username = user?.displayName;
+      final newPost =
+          await FirebaseFirestore.instance.collection('Post Blog').add({
+        'userId': userId,
+        'judul': judul,
+        'isi_blog': isiBlog,
+        'author': username,
+        'image': '',
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      final String id = newPost.id;
+      final ref =
+          FirebaseStorage.instance.ref().child('post_images').child('$id.jpg');
+      final uploadTask = ref.putFile(File(selectedImagePath));
+      final snapshot = await uploadTask.whenComplete(() => null);
+      final image = await snapshot.ref.getDownloadURL();
+      await FirebaseFirestore.instance.collection('Post Blog').doc(id).update({
+        'id': id,
+        'image': image,
+      });
+      FocusScope.of(context).unfocus();
+      showDialog(
+        context: context,
+        builder: (context) => const SuccessDialog(
+            message: 'Postingan Blog Mu Berhasil di Upload!'),
+      );
+    } catch (error) {
+      showDialog(
+        context: context,
+        builder: (context) =>
+            FailedDialog(message: 'Terjadi kesalahan: $error'),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
@@ -39,14 +87,14 @@ class AddPostScreenState extends State<AddPostScreen> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
-              key: _formKey,
+              key: formKey,
               child: Column(
                 children: [
                   (selectedImagePath != '')
                       ? Image.file(
                           File(selectedImagePath),
-                          height: 120,
-                          width: 120,
+                          height: 200,
+                          width: 280,
                           fit: BoxFit.fill,
                         )
                       : Image.asset(
@@ -68,6 +116,17 @@ class AddPostScreenState extends State<AddPostScreen> {
                   TextFormField(
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(), labelText: 'Judul Blog'),
+                    onChanged: (value) {
+                      judul = value.trim();
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Isi Judul Blog Anda!';
+                      } else if (value.length < 10) {
+                        return 'Judul minimal harus 10 karakter!';
+                      }
+                      return null;
+                    },
                   ),
                   const SizedBox(height: 30),
                   TextFormField(
@@ -75,12 +134,25 @@ class AddPostScreenState extends State<AddPostScreen> {
                     maxLines: 10,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(), labelText: 'Isi Post'),
+                    onChanged: (value) {
+                      isiBlog = value.trim();
+                    },
+                    validator: (value) {
+                      {
+                        if (value == null || value.isEmpty) {
+                          return 'Isi post Anda!';
+                        }
+                        return null;
+                      }
+                    },
                   ),
                   const SizedBox(height: 30),
                   CustomButton(
                     text: 'Post',
                     onPressed: () {
-                      Navigator.pop(context);
+                      if (formKey.currentState!.validate()) {
+                        addNewPost();
+                      }
                     },
                     color: const Color(0xffFE0002),
                   ),
