89920d04e8a70b916d2acbdf8d64866060b7ac20
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-18 13:26:36 +0700

    rename news detail screen to post detail screen

---
+++ lib/screen/post_detail_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class PostDetailScreen extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String author;
+
+  const PostDetailScreen({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.author,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'Blog',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final maxWidth = constraints.maxWidth;
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                      width: maxWidth,
+                      height: maxWidth * 0.6,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'By $author',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          description,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
