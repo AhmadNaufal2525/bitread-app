48ba3aa9fd354ff3b432a3c18e2c52aa69ea65a6
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-18 13:28:43 +0700

    changes news card to post card

---
+++ lib/widget/post_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String author;
+  final String imageUrl;
+  const PostCard({super.key, required this.title, required this.description, required this.imageUrl, required this.author, });
+
+    @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Card(
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              imageUrl,
+              fit: BoxFit.fitWidth,
+              height: 160,
+              width: double.infinity,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(19.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Oleh: $author',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
