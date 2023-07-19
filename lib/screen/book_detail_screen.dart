72ea9df06b2e26204f0fd45549a10aae886d3773
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-19 10:19:09 +0700

    update book detail screen

--- lib/screen/book_detail_screen.dart
+++ lib/screen/book_detail_screen.dart
@@ -18,134 +18,131 @@ class BookDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Stack(
-                children: <Widget>[
-                  Image.asset(
-                    imageUrl,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Image.asset(
+                  imageUrl,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fitWidth,
+                ),
+                Positioned(
+                  top: 30,
+                  left: 0,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
                     ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          title,
+                          style: const TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 21),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        author,
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          const StarRating(
+                            rating: 5,
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            rating,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 0, 133, 69),
+                              fontSize: 14,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    "“The Little Mermaid” is a fairy tale written by the Danish author Hans Christian Andersen. The story follows the journey of a young mermaid who is willing to give up her life in the sea as a mermaid to gain a human soul. The tale was first published in 1837 as part of a collection of fairy tales for children.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      letterSpacing: 0.6,
+                      wordSpacing: 0.6,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: CustomButton(
+                          onPressed: () {},
+                          text: 'Baca Buku',
+                          icon: const Icon(Icons.menu_book),
+                          color: const Color(0xffFE0002),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: CustomButton(
+                          onPressed: () {},
+                          text: 'Beli Buku',
+                          icon: const Icon(Icons.trolley),
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Text(
-                            title,
-                            style: const TextStyle(
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 21),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          author,
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        const Spacer(),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            const StarRating(
-                              rating: 5,
-                            ),
-                            const SizedBox(
-                              height: 6,
-                            ),
-                            Text(
-                              rating,
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 0, 133, 69),
-                                fontSize: 14,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      "“The Little Mermaid” is a fairy tale written by the Danish author Hans Christian Andersen. The story follows the journey of a young mermaid who is willing to give up her life in the sea as a mermaid to gain a human soul. The tale was first published in 1837 as part of a collection of fairy tales for children.",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
-                        letterSpacing: 0.6,
-                        wordSpacing: 0.6,
-                      ),
-                      textAlign: TextAlign.justify,
-                    ),
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: CustomButton(
-                            onPressed: () {},
-                            text: 'Baca Buku',
-                            icon: const Icon(Icons.menu_book),
-                            color: const Color(0xffFE0002),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Expanded(
-                          child: CustomButton(
-                            onPressed: () {},
-                            text: 'Beli Buku',
-                            icon: const Icon(Icons.trolley),
-                            color: Colors.grey,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
