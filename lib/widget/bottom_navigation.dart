ec81642082363e40f03d623540220de89d781bd1
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-31 15:17:39 +0700

    update bottom_navigation

--- lib/widget/bottom_navigation.dart
+++ lib/widget/bottom_navigation.dart
@@ -2,7 +2,10 @@ import 'package:bitread_app/screen/home_screen.dart';
 import 'package:bitread_app/screen/post_screen.dart';
 import 'package:bitread_app/screen/profile_screen.dart';
 import 'package:bitread_app/screen/search_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -17,6 +20,7 @@ class _BottomNavState extends State<BottomNav> {
     initialPage: 0,
     keepPage: true,
   );
+
   void pageChanged(int index) {
     setState(() {
       selectedIndex = index;
@@ -26,35 +30,20 @@ class _BottomNavState extends State<BottomNav> {
   void bottomTapped(int index) {
     setState(() {
       selectedIndex = index;
-      pageController.animateToPage(index,
-          duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+      pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.easeInOut,
+      );
     });
   }
 
-  List<BottomNavigationBarItem> buildBottomNavBarItems() {
-    return [
-      const BottomNavigationBarItem(
-        icon: Icon(
-          Icons.home_filled,
-        ),
-        label: 'Home',
-      ),
-      const BottomNavigationBarItem(
-        icon: Icon(
-          Icons.search_rounded,
-        ),
-        label: 'Search',
-      ),
-      const BottomNavigationBarItem(
-        icon: Icon(
-          Icons.newspaper_rounded,
-        ),
-        label: 'Blog',
-      ),
-      const BottomNavigationBarItem(
-        icon: Icon(Icons.person_2_rounded),
-        label: 'Profile',
-      ),
+  List<Widget> buildPages() {
+    return const <Widget>[
+      HomeScreen(),
+      SearchSreen(),
+      PostScreen(),
+      ProfilScreen(),
     ];
   }
 
@@ -67,25 +56,66 @@ class _BottomNavState extends State<BottomNav> {
         onPageChanged: (index) {
           pageChanged(index);
         },
-        children: const <Widget>[
-          HomeScreen(),
-          SearchSreen(),
-          PostScreen(),
-          ProfilScreen(),
-        ],
+        children: buildPages(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: selectedIndex,
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: const Color(0xffFE0002),
+        buttonBackgroundColor: const Color(0xffFE0002),
+        height: 60,
+        animationDuration: const Duration(milliseconds: 300),
+        animationCurve: Curves.easeInOut,
+        index: selectedIndex,
         onTap: (index) {
           bottomTapped(index);
         },
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: const Color(0xffFE0002),
         items: buildBottomNavBarItems(),
       ),
     );
+  }
+
+  List<Widget> buildBottomNavBarItems() {
+    return [
+      const Icon(
+        Icons.home_rounded,
+        size: 30,
+        color: Colors.white,
+      ),
+      const Icon(
+        Icons.search_rounded,
+        size: 30,
+        color: Colors.white,
+      ),
+      const Icon(
+        Icons.newspaper_rounded,
+        size: 30,
+        color: Colors.white,
+      ),
+      StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance
+            .collection('User')
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.hasData && snapshot.data!.exists) {
+            var userData = snapshot.data!.data();
+            String? imageURL = userData?['image'];
+            if (imageURL != null && imageURL.isNotEmpty) {
+              return CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.grey,
+                backgroundImage: NetworkImage(imageURL),
+              );
+            }
+          }
+          return const Icon(
+            Icons.person_2_rounded,
+            size: 30,
+            color: Colors.white,
+          );
+        },
+      )
+    ];
   }
 }
