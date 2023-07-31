a603d219de427d2d44ac9087ad7e64c57fe25055
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-31 15:20:13 +0700

    add searchbox

--- lib/widget/searchbox.dart
+++ lib/widget/searchbox.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SearchBox extends StatefulWidget {
   final Function(String) onSearch;
 
-  const SearchBox({super.key, required this.onSearch});
+  const SearchBox({Key? key, required this.onSearch}) : super(key: key);
 
   @override
   SearchBoxState createState() => SearchBoxState();
@@ -11,10 +11,12 @@ class SearchBox extends StatefulWidget {
 
 class SearchBoxState extends State<SearchBox> {
   TextEditingController searchController = TextEditingController();
+  FocusNode searchFocusNode = FocusNode();
 
   @override
   void dispose() {
     searchController.dispose();
+    searchFocusNode.dispose();
     super.dispose();
   }
 
@@ -24,6 +26,10 @@ class SearchBoxState extends State<SearchBox> {
       children: [
         TextField(
           controller: searchController,
+          focusNode: searchFocusNode,
+          onChanged: (query) {
+            setState(() {});
+          },
           decoration: InputDecoration(
             filled: true,
             fillColor: const Color.fromARGB(255, 228, 225, 225),
@@ -40,16 +46,28 @@ class SearchBoxState extends State<SearchBox> {
                 color: Colors.grey,
               ),
             ),
-            suffixIcon: IconButton(
-              onPressed: () {
-                searchController.clear();
-              },
-              icon: const Icon(Icons.clear, color: Colors.grey),
-            ),
+            suffixIcon: searchController.text.isNotEmpty
+                ? IconButton(
+                    onPressed: () {
+                      searchController.clear();
+                    },
+                    icon: const Icon(Icons.clear, color: Colors.grey),
+                  )
+                : null,
           ),
           onSubmitted: (query) {
             widget.onSearch(query);
             searchController.clear();
+          },
+          onTap: () {
+            setState(() {
+              searchFocusNode.requestFocus();
+            });
+          },
+          onEditingComplete: () {
+            setState(() {
+              searchFocusNode.unfocus();
+            });
           },
         )
       ],
