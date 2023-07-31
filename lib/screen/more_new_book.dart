cd6cb8d3c61bd9623516123936936e613142011d
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-07-31 15:12:12 +0700

    update more_new_book

--- lib/screen/more_new_book.dart
+++ lib/screen/more_new_book.dart
@@ -30,27 +30,26 @@ class _NewBookState extends State<NewBook> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'Buku Terbaru',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 80),
-                  child: Text(
-                    'Buku Terbaru',
-                    style: TextStyle(fontWeight: FontWeight.w900, fontSize: 16),
-                  ),
-                )
-              ],
-            ),
+            
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16),
@@ -89,9 +88,9 @@ class _NewBookState extends State<NewBook> {
                                       id: book['id'],
                                       title: book['title'],
                                       author: book['author'],
-                                      rating: book['rating'].toString(),
+                                      rating: book['rating'],
                                       imageUrl: book['imageUrl'],
-                                      desc: book['description']),
+                                      desc: book['description'],url : book['url_book'],),
                                 ),
                               );
                             },
