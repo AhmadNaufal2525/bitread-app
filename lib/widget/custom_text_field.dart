17cf010969119bf8af81410392b12552e2201ca6
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-08-03 18:47:00 +0700

    update custom_text_field

--- lib/widget/custom_text_field.dart
+++ lib/widget/custom_text_field.dart
@@ -8,6 +8,7 @@ class CustomTextField extends StatelessWidget {
   final FormFieldValidator<String>? validator;
   final String? initialValue;
   final TextEditingController? controller;
+  final bool isReadOnly;
 
   const CustomTextField({
     Key? key,
@@ -16,43 +17,50 @@ class CustomTextField extends StatelessWidget {
     this.obscureText = false,
     this.validator,
     this.initialValue,
-    this.controller, required this.onChanged,
+    this.controller,
+    required this.onChanged,
+    this.isReadOnly = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: const Color.fromARGB(255, 235, 231, 231)),
-      ),
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            color: Colors.grey,
-          ),
-          const Expanded(
-            child: SizedBox(
-              height: 24.0,
-              child: VerticalDivider(color: Colors.grey),
-            ),
-          ),
-          Expanded(
-            flex: 8,
-            child: TextFormField(
-              onChanged: onChanged,
-              controller: controller,
-              validator: validator,
-              obscureText: obscureText,
-              decoration: InputDecoration(
-                hintText: hintText,
-                border: InputBorder.none,
-                hintStyle: const TextStyle(color: Colors.grey),
-              ),
-            ),
-          ),
-        ],
+    return TextFormField(
+      readOnly: isReadOnly,
+      initialValue: initialValue,
+      onChanged: onChanged,
+      controller: controller,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: hintText,
+        hintText: hintText,
+        labelStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+        ),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.black,
+          size: 18,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.grey, width: 2),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        floatingLabelStyle: const TextStyle(color: Colors.black, fontSize: 18),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.black, width: 1.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.red, width: 1.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.red, width: 1.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
