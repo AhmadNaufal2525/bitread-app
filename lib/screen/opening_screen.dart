8ceae291b99791e4f33a6f4ebb4f194236f5426a
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-08-03 18:42:47 +0700

    update opening_screen

--- lib/screen/opening_screen.dart
+++ lib/screen/opening_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:bitread_app/screen/login_screen.dart';
 import 'package:bitread_app/screen/register_screen.dart';
 import 'package:bitread_app/widget/custom_button.dart';
@@ -20,73 +21,85 @@ class _OpeningScreenState extends State<OpeningScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Image.asset(
-                'assets/logo_bitread.png',
-                width: 120,
-                height: 120,
-              ),
-              Center(
+              FadeIn(
                 child: Image.asset(
-                  'assets/opening.png',
-                  width: 260,
-                  height: 260,
+                  'assets/logo_bitread.png',
+                  width: 120,
+                  height: 120,
+                ),
+              ),
+              BounceInDown(
+                child: Center(
+                  child: Image.asset(
+                    'assets/opening.png',
+                    width: 260,
+                    height: 260,
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
-              CustomButton(
-                text: 'Login',
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(25),
+              FadeInUp(
+                delay: const Duration(milliseconds: 600),
+                child: CustomButton(
+                  text: 'Login',
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(25),
+                        ),
                       ),
-                    ),
-                    isScrollControlled: true,
-                    builder: (context) {
-                      return const FractionallySizedBox(
-                        heightFactor: 0.862,
-                        child: LoginScreen(),
-                      );
-                    },
-                  );
-                },
-                width: w * 0.7,
-                color: const Color(0xffE97777),
+                      isScrollControlled: true,
+                      builder: (context) {
+                        return const FractionallySizedBox(
+                          heightFactor: 0.862,
+                          child: LoginScreen(),
+                        );
+                      },
+                    );
+                  },
+                  width: w * 0.7,
+                  color: const Color(0xffE97777),
+                ),
               ),
               const SizedBox(height: 24),
-              CustomButton(
-                text: 'Register',
-                onPressed: () {
-                  showModalBottomSheet(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(25),
+              FadeInUp(
+                delay: const Duration(milliseconds: 600),
+                child: CustomButton(
+                  text: 'Register',
+                  onPressed: () {
+                    showModalBottomSheet(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(25),
+                        ),
                       ),
-                    ),
-                    isScrollControlled: true,
-                    context: context,
-                    builder: (context) {
-                      return const FractionallySizedBox(
-                        heightFactor: 0.862,
-                        child: RegisterScreen(),
-                      );
-                    },
-                  );
-                },
-                width: w * 0.7,
-                color: Colors.white,
-                textColor: Colors.black,
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (context) {
+                        return const FractionallySizedBox(
+                          heightFactor: 0.862,
+                          child: RegisterScreen(),
+                        );
+                      },
+                    );
+                  },
+                  width: w * 0.7,
+                  color: Colors.white,
+                  textColor: Colors.black,
+                ),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.25,
               ),
-              const Text(
-                'Bitread All Right Reserved @2023',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
+              FadeIn(
+                child: const Text(
+                  'Bitread All Right Reserved @2023',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
                 ),
               ),
             ],
