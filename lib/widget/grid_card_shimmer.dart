059d7bcb1e1b3f17df6ca2a8f01a3a399e4cabdb
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-08-03 18:47:47 +0700

    add grid_card_shimmer

---
+++ lib/widget/grid_card_shimmer.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class GridCardBookShimmer extends StatelessWidget {
+  const GridCardBookShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 100,
+                  height: 12,
+                  color: Colors.grey[300],
+                ),
+              ),
+              const SizedBox(height: 8),
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 100,
+                  height: 12,
+                  color: Colors.grey[300],
+                ),
+              ),
+              const SizedBox(height: 8),
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 50,
+                  height: 12,
+                  color: Colors.grey[300],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
