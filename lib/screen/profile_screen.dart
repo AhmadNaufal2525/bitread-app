70e5034767544c089558ed3916a9e3cdfc62df8e
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-08-03 18:43:51 +0700

    update profile_screen

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:bitread_app/screen/opening_screen.dart';
+import 'package:bitread_app/widget/grid_profile_post.dart';
 import 'package:flutter/material.dart';
 import 'package:bitread_app/widget/profile_header_widget.dart';
-import 'package:bitread_app/widget/profile_liked_post.dart';
-import 'package:bitread_app/widget/profile_post.dart';
+import 'package:bitread_app/widget/list_profile_post.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -16,7 +16,6 @@ class ProfilScreen extends StatefulWidget {
 
 class _ProfilScreenState extends State<ProfilScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
-  int postCount = 0;
   GoogleSignIn googleSignIn = GoogleSignIn();
   Future<void> setLoggedIn(bool isLoggedIn) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -25,12 +24,16 @@ class _ProfilScreenState extends State<ProfilScreen> {
 
   Future<void> logout() async {
     await setLoggedIn(false);
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) => const OpeningScreen(),
-      ),
-      (route) => false,
+    setState(
+      () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => const OpeningScreen(),
+          ),
+          (route) => false,
+        );
+      },
     );
     await auth.signOut();
     await googleSignIn.signOut();
@@ -39,68 +42,76 @@ class _ProfilScreenState extends State<ProfilScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              child: Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.logout,
-                    color: Color(0xffFE0002),
-                  ),
-                  onPressed: logout,
-                ),
-              ),
-            ),
-            Expanded(
-              child: DefaultTabController(
-                length: 2,
-                child: NestedScrollView(
-                  headerSliverBuilder: (context, _) {
-                    return [
-                      SliverList(
-                        delegate: SliverChildListDelegate(
-                          [
-                            const ProfileHeader(),
-                          ],
-                        ),
-                      ),
-                    ];
-                  },
-                  body: Column(
-                    children: [
-                      Material(
-                        color: Colors.white,
-                        child: TabBar(
-                          labelColor: Colors.black,
-                          unselectedLabelColor: Colors.grey[400],
-                          indicatorWeight: 1,
-                          indicatorColor: const Color(0xffFE0002),
-                          tabs: const [
-                            Tab(
-                              text: 'Post',
-                            ),
-                            Tab(
-                              text: 'Liked Post',
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Expanded(
-                        child: TabBarView(
-                          children: [ProfilePost(), ProfileLikedPost()],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+      appBar: AppBar(
+        toolbarHeight: 46,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.transparent,
+        centerTitle: true,
+        title: const Text(
+          'Profile',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w800),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Color(0xffFE0002),
+            ),
+            onPressed: logout,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: DefaultTabController(
+              length: 2,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, _) {
+                  return [
+                    SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          const ProfileHeader(),
+                        ],
+                      ),
+                    ),
+                  ];
+                },
+                body: Column(
+                  children: [
+                    Material(
+                      color: Colors.white,
+                      child: TabBar(
+                        labelColor: Colors.black,
+                        unselectedLabelColor: Colors.grey[400],
+                        indicatorWeight: 2,
+                        indicatorColor: const Color(0xffFE0002),
+                        tabs: const [
+                          Tab(
+                            icon: Icon(Icons.view_list_rounded),
+                          ),
+                          Tab(
+                            icon: Icon(Icons.grid_view_rounded),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Expanded(
+                      child: TabBarView(
+                        children: [ListProfilePost(), GridProfilePost()],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
