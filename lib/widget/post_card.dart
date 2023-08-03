8ee780db5f4e91758c6866ec1a7639ca864faa2b
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-08-03 18:49:33 +0700

    update post_card

--- lib/widget/post_card.dart
+++ lib/widget/post_card.dart
@@ -5,13 +5,14 @@ class PostCard extends StatelessWidget {
   final String description;
   final String author;
   final String imageUrl;
+
   const PostCard({
-    super.key,
+    Key? key,
     required this.title,
     required this.description,
     required this.imageUrl,
     required this.author,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,43 +20,49 @@ class PostCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 12.0),
       child: Card(
         elevation: 5,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              imageUrl,
-              fit: BoxFit.fitWidth,
-              height: 160,
-              width: double.infinity,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(19.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    description,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Oleh: $author',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                imageUrl,
+                fit: BoxFit.fitWidth,
+                height: 160,
+                width: double.infinity,
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(19.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      description,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Oleh: $author',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
