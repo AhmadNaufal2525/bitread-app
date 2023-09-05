a2191aa1e37968ebbd206af6571769a0c445d85f
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-09-05 15:43:23 +0700

    update opening_screen.dart

--- lib/screen/opening/opening_screen.dart
+++ lib/screen/opening/opening_screen.dart
@@ -15,127 +15,131 @@ class OpeningScreen extends StatefulWidget {
 class _OpeningScreenState extends State<OpeningScreen> {
   @override
   Widget build(BuildContext context) {
-    double w = MediaQuery.of(context).size.width;
-    double h = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color(0xffFE0002),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: h * 0.03,
-            ),
-            FadeIn(
-              child: Image.asset(
-                'assets/logo_bitread.png',
-                width: 120,
-                height: 120,
-              ),
-            ),
-            BounceInDown(
-              child: Center(
-                child: Image.asset(
-                  'assets/opening.png',
-                  width: 260,
-                  height: 260,
-                ),
-              ),
-            ),
-            SizedBox(height: h * 0.03),
-            FadeInUp(
-              delay: const Duration(milliseconds: 600),
-              child: CustomButton(
-                text: 'Login',
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(25),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            double w = constraints.maxWidth;
+            double h = constraints.maxHeight;
+
+            return SingleChildScrollView(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 20),
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.info,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const About(),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                       ),
-                    ),
-                    isScrollControlled: true,
-                    builder: (context) {
-                      return const FractionallySizedBox(
-                        heightFactor: 0.862,
-                        child: LoginScreen(),
-                      );
-                    },
-                  );
-                },
-                width: w * 0.7,
-                color: const Color(0xffE97777),
-              ),
-            ),
-            SizedBox(height: h * 0.03),
-            FadeInUp(
-              delay: const Duration(milliseconds: 600),
-              child: CustomButton(
-                text: 'Register',
-                onPressed: () {
-                  showModalBottomSheet(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(25),
+                      FadeIn(
+                        child: Image.asset(
+                          'assets/logo_bitread.png',
+                          width: w * 0.4, // Sesuaikan ukuran gambar
+                          height: w * 0.4,
+                        ),
                       ),
-                    ),
-                    isScrollControlled: true,
-                    context: context,
-                    builder: (context) {
-                      return const FractionallySizedBox(
-                        heightFactor: 0.862,
-                        child: RegisterScreen(),
-                      );
-                    },
-                  );
-                },
-                width: w * 0.7,
-                color: Colors.white,
-                textColor: Colors.black,
-              ),
-            ),
-            SizedBox(
-              height: h * 0.16,
-            ),
-            FadeInUp(
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                      BounceInDown(
+                        child: Center(
+                          child: Image.asset(
+                            'assets/opening.png',
+                            width: w * 0.6, // Sesuaikan ukuran gambar
+                            height: w * 0.6,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: h * 0.06),
+                      FadeInUp(
+                        delay: const Duration(milliseconds: 600),
+                        child: CustomButton(
+                          text: 'Login',
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(25),
+                                ),
+                              ),
+                              isScrollControlled: true,
+                              builder: (context) {
+                                return const FractionallySizedBox(
+                                  heightFactor: 0.862,
+                                  child: LoginScreen(),
+                                );
+                              },
+                            );
+                          },
+                          width: w * 0.67,
+                          height: h * 0.07,
+                          color: const Color(0xffE97777),
+                        ),
+                      ),
+                      SizedBox(height: h * 0.03),
+                      FadeInUp(
+                        delay: const Duration(milliseconds: 600),
+                        child: CustomButton(
+                          text: 'Register',
+                          onPressed: () {
+                            showModalBottomSheet(
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(25),
+                                ),
+                              ),
+                              isScrollControlled: true,
+                              context: context,
+                              builder: (context) {
+                                return const FractionallySizedBox(
+                                  heightFactor: 0.862,
+                                  child: RegisterScreen(),
+                                );
+                              },
+                            );
+                          },
+                          width: w * 0.67,
+                          height: h * 0.07,
+                          color: Colors.white,
+                          textColor: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: h * 0.1,
+                      ),
+                      FadeIn(
+                        child: const Expanded(
+                          child: Text(
+                            'Bitread All Right Reserved @2023',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const About(),
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.group,
-                  color: Colors.black,
-                ),
-                label: const Text(
-                  'Tentang Kami',
-                  style: TextStyle(color: Colors.black),
-                ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: h * 0.04,
-            ),
-            FadeIn(
-              child: const Text(
-                'Bitread All Right Reserved @2023',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
